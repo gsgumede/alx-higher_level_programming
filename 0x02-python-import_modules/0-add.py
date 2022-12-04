1cc18a290227c4e4d4c35fd19753788e215864a4
George Gumede <gumedegeorge6@gmail.com>
2022-12-04 15:48:23 +0200

    Fix: 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-import add_0
-
-
-def main():
-
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add_0.add(a,b)}")
-
+from add_0 import add
 
 if __name__ == "__main__":
-    main()
+    """
+    Prints the result of the addition between two numbers
+    """
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
