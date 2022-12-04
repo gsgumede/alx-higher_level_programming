bdc311102d3e3ec8fb4c604a401279a70710e5f9
George Gumede <gumedegeorge6@gmail.com>
2022-12-04 15:53:18 +0200

    Add:

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
+
+def main():
     """
     Prints the result of the addition between two numbers
     """
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
