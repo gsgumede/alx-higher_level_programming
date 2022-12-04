26587307bb3d5d54e50c2503c2580af118ff02e5
George Gumede <gumedegeorge6@gmail.com>
2022-12-04 17:20:59 +0200

    Add:2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,15 @@
 import sys
 
 
-n = len(sys.argv)
-if n > 1:
-    print("{:d} arguments:".format(n - 1))
-    for index in range(1, n):
-        print("{:d}: {:s}".format(index, sys.argv[index]))
-else:
-    print("{:d} arguments.".format(0))
+def main():
+    n = len(sys.argv)
+    if n > 1:
+        print("{:d} arguments:".format(n - 1))
+        for index in range(1, n):
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+    else:
+        print("{:d} arguments.".format(0))
+
+
+if __name__ == "__main__":
+    main()
