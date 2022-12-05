ad38baafe6f60a98bedaeec083100328ec46d564
George Gumede <gumedegeorge6@gmail.com>
2022-12-05 16:30:59 +0200

    Add:5-variable_load_5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import variable_load_5
+
+
+def main():
+    listdir = dir(variable_load_5)
+    for index in range(len(listdir)):
+        if lustdir[index] == "a":
+            print(listdir[index])
+
+
+if __name__ == "__main__":
+    main()
