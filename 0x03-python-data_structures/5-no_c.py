3de0c16d67729f698ffe72090800732ce216913f
George Gumede <gumedegeorge6@gmail.com>
2022-12-07 11:33:38 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,11 @@
 
 
 def no_c(my_string):
-    new_string = my_string[:]
-    return new_string
+    if my_string:
+        my_string = list(my_string)
+        n = len(my_string)
+        for index in range(n):
+            if my_string[index] == "c" or my_string[index] == "C":
+                my_string[index] = " "
+        my_string = "".join(my_string)
+        return my_string
