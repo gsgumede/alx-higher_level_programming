2e4bf67cd4310a3ba12a469bcfc8c19bd42393b1
George Gumede <gumedegeorge6@gmail.com>
2022-12-07 14:08:01 +0200

    Fix:6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for index in matrix:
             for i in index:
-                print("{:d} ".format(i), end="")
+                print("{:d}".format(i), end=" ")
             print(end='\n')
