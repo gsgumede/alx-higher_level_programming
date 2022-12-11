43ab3613352bb30415fa14872dcbafc2f01ba68a
George Gumede <gumedegeorge6@gmail.com>
2022-12-11 10:23:07 +0200

    Fix:5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-for index in range(3):
-    print("{:s}".format(str), end="")
-print()
+print(f"{str:s}" * 3)
 slice_str = str[:9]
-print("{:s}".format(slice_str))
+print(f"{slice_str:s}")
