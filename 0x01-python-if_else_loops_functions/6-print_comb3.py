fbf4dbb6c5120d799b0b57da974b8cd024fa3294
George Gumede <gumedegeorge6@gmail.com>
2022-12-15 01:50:03 +0200

    Add:6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 < 8:
-            print("{:d}{:d}".format(digit1, digit2), end="")
+            print("{:d}{:d}, ".format(digit1, digit2), end="")
         else:
-            print("{:d}{:d}".format(digit1, digit2), end="")
+            print("{:d}{:d}".format(digit1, digit2))
