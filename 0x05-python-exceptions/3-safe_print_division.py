17acd69c9d3f879d2842dc231133279d202b4319
George Gumede <gumedegeorge6@gmail.com>
2022-12-21 12:19:40 +0200

    Add:3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        resul = a / b
+    except ZeroDivisionError:
+        resul = None
+    finally:
+        print("Inside Result: {}".format(resul))
+        return (resul)
