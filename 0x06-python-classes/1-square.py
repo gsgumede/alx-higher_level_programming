0012738917ba72d272ca9013700f9c1ad75acfe8
George Gumede <gumedegeorge6@gmail.com>
2022-12-26 12:42:11 +0200

    Add:1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represent a square class"""
+
+    def __init__(self, size):
+        """Initialise a square instance
+
+        Args:
+        size (int): The size of a new square
+        """
+        self.__size = size
